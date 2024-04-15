e85c801a9b9e3bacc82b62eac71c2c4be6c4f93c
rabiaehsan <44220680+rabiaehsan@users.noreply.github.com>
2024-04-14 23:28:33 -1200

    Create api.dart

---
+++ lib/api.dart
@@ -0,0 +1,14 @@
+[
+  {
+    "name": "Football",
+    "description": "This is a description of football."
+  },
+  {
+    "name": "Basketball",
+    "description": "This is a description of basketball."
+  },
+  {
+    "name": "Tennis",
+    "description": "This is a description of tennis."
+  }
+]
