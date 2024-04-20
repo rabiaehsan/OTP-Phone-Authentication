043826970ab092a5482ba6cea9957a9625ec304d
rabiaehsan <44220680+rabiaehsan@users.noreply.github.com>
2024-04-20 04:53:14 -1200

    Create slotapi.dart

---
+++ lib/slotapi.dart
@@ -0,0 +1,14 @@
+[
+  {
+    "start_time": "2:00 PM",
+    "end_time": "3:00 PM"
+  },
+  {
+    "start_time": "3:00 PM",
+    "end_time": "4:00 PM"
+  },
+  {
+    "start_time": "4:00 PM",
+    "end_time": "5:00 PM"
+  }
+]
