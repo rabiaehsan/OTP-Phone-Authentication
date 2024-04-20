18bc19039e8513d62627fd306ddc724e1a032adc
rabiaehsan <44220680+rabiaehsan@users.noreply.github.com>
2024-04-19 20:32:30 -1200

    Update api.dart

--- lib/api.dart
+++ lib/api.dart
@@ -1,14 +1,18 @@
 [
   {
     "name": "Football",
-    "description": "This is a description of football."
+    "description": "This is a description of football.",
+    "image": "https://unsplash.com/photos/an-aerial-view-of-a-soccer-field-in-the-mountains-GMFsiHka85I"
   },
   {
     "name": "Basketball",
-    "description": "This is a description of basketball."
+    "description": "This is a description of basketball.",
+    "image": "https://unsplash.com/photos/an-aerial-view-of-a-soccer-field-in-the-mountains-GMFsiHka85I"
   },
   {
     "name": "Tennis",
-    "description": "This is a description of tennis."
+    "description": "This is a description of tennis.",
+    "image": "https://unsplash.com/photos/an-aerial-view-of-a-soccer-field-in-the-mountains-GMFsiHka85I"
   }
 ]
+
