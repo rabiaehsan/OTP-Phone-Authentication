3a5ab79776774332146b36497614c435e38f34f0
rabiaehsan <44220680+rabiaehsan@users.noreply.github.com>
2024-04-20 05:53:26 -1200

    Update slotapi.dart

--- lib/slotapi.dart
+++ lib/slotapi.dart
@@ -1,14 +1,5 @@
 [
-  {
-    "start_time": "2:00 PM",
-    "end_time": "3:00 PM"
-  },
-  {
-    "start_time": "3:00 PM",
-    "end_time": "4:00 PM"
-  },
-  {
-    "start_time": "4:00 PM",
-    "end_time": "5:00 PM"
-  }
+  {"start_time": "2:00 PM", "end_time": "3:00 PM", "price": "2400 Rs"},
+  {"start_time": "3:00 PM", "end_time": "4:00 PM", "price": "2800 Rs"},
+  {"start_time": "4:00 PM", "end_time": "5:00 PM", "price": "3000 Rs"}
 ]
