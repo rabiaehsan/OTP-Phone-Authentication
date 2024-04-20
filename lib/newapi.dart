c9541dfdce462f7175e10fd1d5f8d0c86feab871
rabiaehsan <44220680+rabiaehsan@users.noreply.github.com>
2024-04-20 01:54:47 -1200

    Create newapi.dart

---
+++ lib/newapi.dart
@@ -0,0 +1,32 @@
+[
+  {
+    "name": "FootSall",
+    "description": "This is a description of football.",
+    "image": "https://unsplash.com/photos/an-aerial-view-of-a-soccer-field-in-the-mountains-GMFsiHka85I"
+  },
+  {
+    "name": "Basketball",
+    "description": "This is a description of basketball.",
+    "image": "https://unsplash.com/photos/an-aerial-view-of-a-soccer-field-in-the-mountains-GMFsiHka85I"
+  },
+  {
+    "name": "Tennis",
+    "description": "This is a description of tennis.",
+    "image": "https://unsplash.com/photos/an-aerial-view-of-a-soccer-field-in-the-mountains-GMFsiHka85I"
+  },
+  {
+    "name": "FootSall",
+    "description": "This is a description of football.",
+    "image": "https://unsplash.com/photos/an-aerial-view-of-a-soccer-field-in-the-mountains-GMFsiHka85I"
+  },
+  {
+    "name": "Basketball",
+    "description": "This is a description of basketball.",
+    "image": "https://unsplash.com/photos/an-aerial-view-of-a-soccer-field-in-the-mountains-GMFsiHka85I"
+  },
+  {
+    "name": "Tennis",
+    "description": "This is a description of tennis.",
+    "image": "https://unsplash.com/photos/an-aerial-view-of-a-soccer-field-in-the-mountains-GMFsiHka85I"
+  }
+]
