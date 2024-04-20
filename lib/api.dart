585b8463f2d3525d3702fd442222f23fa607a322
rabiaehsan <44220680+rabiaehsan@users.noreply.github.com>
2024-04-20 00:30:09 -1200

    Update api.dart

--- lib/api.dart
+++ lib/api.dart
@@ -14,16 +14,7 @@
     "description": "This is a description of tennis.",
     "image": "https://unsplash.com/photos/an-aerial-view-of-a-soccer-field-in-the-mountains-GMFsiHka85I"
   }
-  {
-    "name": "Circket",
-    "description": "This is a description of football.",
-    "image": "https://unsplash.com/photos/an-aerial-view-of-a-soccer-field-in-the-mountains-GMFsiHka85I"
-  },
-  {
-    "name": "Basketball",
-    "description": "This is a description of basketball.",
-    "image": "https://unsplash.com/photos/an-aerial-view-of-a-soccer-field-in-the-mountains-GMFsiHka85I"
-  },
+
   
 ]
 
