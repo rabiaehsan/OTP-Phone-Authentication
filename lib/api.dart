6ed218cf2bfa2754878ceb5d9851f5ae5a4e8521
rabiaehsan <44220680+rabiaehsan@users.noreply.github.com>
2024-04-28 18:11:29 -1200

    Update api.dart

--- lib/api.dart
+++ lib/api.dart
@@ -10,23 +10,28 @@
     "image": "https://unsplash.com/photos/an-aerial-view-of-a-soccer-field-in-the-mountains-GMFsiHka85I"
   },
   {
-    "name": "Tennis",
-    "description": "This is a description of tennis.",
+    "name": "Cricket Nets",
+    "description": "This is a description of Cricket Nets.",
     "image": "https://unsplash.com/photos/an-aerial-view-of-a-soccer-field-in-the-mountains-GMFsiHka85I"
   },
   {
-    "name": "FootSall",
-    "description": "This is a description of football.",
+    "name": "Gym",
+    "description": "This is a description of the Gym.",
     "image": "https://unsplash.com/photos/an-aerial-view-of-a-soccer-field-in-the-mountains-GMFsiHka85I"
   },
   {
-    "name": "Basketball",
-    "description": "This is a description of basketball.",
+    "name": "Badminton",
+    "description": "This is a description of Badminton.",
     "image": "https://unsplash.com/photos/an-aerial-view-of-a-soccer-field-in-the-mountains-GMFsiHka85I"
   },
   {
-    "name": "Tennis",
-    "description": "This is a description of tennis.",
+    "name": "Walking Track",
+    "description": "This is a description of Walking Track.",
+    "image": "https://unsplash.com/photos/an-aerial-view-of-a-soccer-field-in-the-mountains-GMFsiHka85I"
+  },
+  {
+    "name": "jitsu",
+    "description": "This is a description of jitsu.",
     "image": "https://unsplash.com/photos/an-aerial-view-of-a-soccer-field-in-the-mountains-GMFsiHka85I"
   }
 ]
