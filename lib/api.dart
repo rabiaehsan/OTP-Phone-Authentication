c392351948b7772a88759d1c437217955e02e399
rabiaehsan <44220680+rabiaehsan@users.noreply.github.com>
2024-06-13 18:21:24 -1200

    Update NEW api.dart

--- lib/api.dart
+++ lib/api.dart
@@ -1,33 +1,19 @@
 [
   {
-    "name": "FutSall",
-    "description": "This is a description of football.",
-    "image": "https://unsplash.com/photos/an-aerial-view-of-a-soccer-field-in-the-mountains-GMFsiHka85I",
-    "reservationType": "hourly"
+    "name": "CIRCKET",
+    
   },
   {
-    "name": "Cricket futsal",
-    "description": "This is a description of basketball.",
-    "image": "https://unsplash.com/photos/an-aerial-view-of-a-soccer-field-in-the-mountains-GMFsiHka85I",
-    "reservationType": "daily"
+    "name": "FUTSALL",
+    
   },
   {
-    "name": "Cricket Nets",
-    "description": "This is a description of Cricket Nets.",
-    "image": "https://unsplash.com/photos/an-aerial-view-of-a-soccer-field-in-the-mountains-GMFsiHka85I",
-    "reservationType": "weekly"
-  },
-  {
-    "name": "Gym",
-    "description": "This is a description of the Gym.",
-    "image": "https://unsplash.com/photos/an-aerial-view-of-a-soccer-field-in-the-mountains-GMFsiHka85I",
-    "reservationType": "monthly"
+    "name": "GYM",
+    
   },
   {
     "name": "Badminton",
-    "description": "This is a description of Badminton.",
-    "image": "https://unsplash.com/photos/an-aerial-view-of-a-soccer-field-in-the-mountains-GMFsiHka85I",
-    "reservationType": "hourly"
+   
   },
   {
     "name": "Walking Track",
