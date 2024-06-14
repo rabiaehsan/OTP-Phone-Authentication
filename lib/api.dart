ef341bd3f0116448bacc38f2bcaf99ffbe750551
rabiaehsan <44220680+rabiaehsan@users.noreply.github.com>
2024-06-13 18:23:34 -1200

    Update api.dart

--- lib/api.dart
+++ lib/api.dart
@@ -17,8 +17,6 @@
   },
   {
     "name": "Walking Track",
-    "description": "This is a description of Walking Track.",
-    "image": "https://unsplash.com/photos/an-aerial-view-of-a-soccer-field-in-the-mountains-GMFsiHka85I",
-    "reservationType": "daily"
+
   }
 ]
